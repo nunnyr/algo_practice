be0588ddc10284e34f961794df4d7dedbc3ae0a3
Nunny R <nunnyreyes@gmail.com>
2020-12-09 17:07:46 -0500

    frequency

--- section_six/index.js
+++ section_six/index.js
@@ -1,1 +1,18 @@
 //edit 
+function sameFrequency(first, second){
+    //split both number 
+    //make it into a map (only the first one)
+    //subtract value from the map. if the map has a value of 1 remaining........it does not have the same frequency of digits
+  // good luck. Add any arguments you deem necessary.
+  
+  let strOne = first.toString()
+  let strTwo = second.toString()
+  let arrOne = strOne.split(" ")
+  let arrTwo = strTwo.toString(" ")
+  
+  console.log("this is arr:", arrOne)
+
+
+}
+
+console.log(sameFrequency(814, 418))
